058025282d85f00dedda8d6005e6493d90b7b07a
GIRARDIN Claire <116156608+GirardinClaire@users.noreply.github.com>
2023-03-14 13:58:13 +0100

    Update connect.php

--- EscapeGame/php/connect.php
+++ EscapeGame/php/connect.php
@@ -2,8 +2,8 @@
 
 // PHP de connexion à la base de données AlwaysData avec le compte de Claire Girardin
 
-//$link = pg_connect("host=postgresql-girardinclaire.alwaysdata.net dbname=girardinclaire_bddescapegame user=girardinclaire password=wT5RySfX#JE");
-$link = mysqli_connect("mysql-girardinclaire.alwaysdata.net", "292688", "wT5RySfX#JE", "girardinclaire_bddescapegame");
+//$link = pg_connect("host=postgresql-girardinclaire.alwaysdata.net dbname=girardinclaire_bddescapegame user=girardinclaire password=***");
+$link = mysqli_connect("mysql-girardinclaire.alwaysdata.net", username, password, "girardinclaire_bddescapegame");
 
 if (!$link) {
     die('Erreur de connexion');
